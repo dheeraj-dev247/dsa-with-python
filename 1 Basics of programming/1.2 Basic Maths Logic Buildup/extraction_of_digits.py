1ab1df224d5cd3aa87f4c5758d2c2315380cde49
Dheeraj Malik <160769833+dheeraj-dev247@users.noreply.github.com>
2025-11-13 10:35:23 +0000

    extraction of digits done

--- 1 Basics of programming/1.2 Basic Maths Logic Buildup/extraction_of_digits.py
+++ 1 Basics of programming/1.2 Basic Maths Logic Buildup/extraction_of_digits.py
@@ -1,1 +1,7 @@
-print("hello")
+def extraction_digit(num):
+    while num > 0:
+        last_digit = num%10
+        print(f"Last Digit is {last_digit}")
+        num = num//10
+
+extraction_digit(154)
