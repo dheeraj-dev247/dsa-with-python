f36161071e67709f3a8b393709697b532ff73b2e
Dheeraj Malik <160769833+dheeraj-dev247@users.noreply.github.com>
2025-11-14 10:45:31 +0000

    print factor done with all three approach

--- 1 Basics of programming/1.2 Basic Maths Logic Buildup/1.2.5 print_factors_of_number.py
+++ 1 Basics of programming/1.2 Basic Maths Logic Buildup/1.2.5 print_factors_of_number.py
@@ -3,7 +3,38 @@
 # 2. Using loop upto half of the number
 # 3. Using sqrt
 
+from math import sqrt
+
 def print_factors_brute_force(num):
-    pass
+    result = []
+    for i in range(1,num+1):
+        if (num%i == 0):
+            result.append(i)
+    return result
+
+print(print_factors_brute_force(36))
 
 
+def print_factor_better_solution(num):
+    n = num//2
+    result = []
+    for i in range(1,n+1):
+        if (num%i == 0):
+            result.append(i)
+    result.append(num)
+    return result
+
+print(print_factor_better_solution(36))
+
+def print_factor_optimal_solution(num):
+    n = int(sqrt(num))
+    result = []
+    for i in range(1,n+1):
+        if(num%i == 0):
+            result.append(i)
+            answer = num//i
+            if(answer != i):
+                result.append(answer)
+    return result
+
+print(print_factor_optimal_solution(48))
