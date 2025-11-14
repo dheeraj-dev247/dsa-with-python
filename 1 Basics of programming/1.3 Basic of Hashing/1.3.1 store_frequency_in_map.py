fe7d9310ba3830a545a2081463b5cf089b7f9d12
Dheeraj Malik <160769833+dheeraj-dev247@users.noreply.github.com>
2025-11-14 11:46:59 +0000

    added the second and simple method

--- 1 Basics of programming/1.3 Basic of Hashing/1.3.1 store_frequency_in_map.py
+++ 1 Basics of programming/1.3 Basic of Hashing/1.3.1 store_frequency_in_map.py
@@ -10,3 +10,14 @@ def store_frequency_of_numbers(nums,x):
     return freq_map[x]
 
 print(store_frequency_of_numbers([1,2,3,1,2,2],2))
+
+
+def store_frequency_of_number_better(nums,x):
+    length = len(nums)
+    freq_map = {}
+    for i in range(0,length):
+        freq_map[nums[i]] = freq_map.get(nums[i],0) + 1
+    print(freq_map)
+    return freq_map[x]
+
+print(store_frequency_of_number_better([1,2,3,1,2,2],2))
