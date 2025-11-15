de33d526837bcd636614b53939ad4c74568eeecd
Dheeraj Malik <160769833+dheeraj-dev247@users.noreply.github.com>
2025-11-15 07:44:04 +0000

    Recusion theory is added

---
+++ 1 Basics of programming/1.4 Recusion Basic/1.4.1 What is Recursion.py
@@ -0,0 +1,10 @@
+# A function will be called again and again but with the basic condition is called the Recusion.
+# Mainly there are 2 Types of Recusion
+# 1. Head Recursion
+# 2. Tail Recursion
+# Head Recursion -> First Job is done -> called the function
+# Tail Recursion -> First function is called -> job is done at last
+# On each function call function will be registered in memory stack and once function is completed will be removed from the memory stack.
+# Try making Recusion tree for better understanding.
+
+
