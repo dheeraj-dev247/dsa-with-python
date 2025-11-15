542511b93329c0ed8e9eece987d8bad2badf7e6f
Dheeraj Malik <160769833+dheeraj-dev247@users.noreply.github.com>
2025-11-15 13:27:54 +0000

    reverse array with recursion is completed

--- 1 Basics of programming/1.4 Recusion Basic/1.4.5 reverse_sub_array.py
+++ 1 Basics of programming/1.4 Recusion Basic/1.4.5 reverse_sub_array.py
@@ -10,17 +10,14 @@ def rev_sub_array_using_loop(left,right,nums):
 print(rev_sub_array_using_loop(2,4, [1, 2, 3, 4, 5, 6, 7])) 
 
 
-
-def rev_sub_array_using_recursion(left,right,nums):
-    left -=1
-    right -=1
-    #Base condition
-    if left > right:
-        return nums
-    while left < right: 
-        nums[left],nums[right] = nums[right],nums[left]
-        left += 1
-        right -= 1
-    return rev_sub_array_using_recursion(left+1,right-1,nums)
-
-print(rev_sub_array_using_recursion(2,4,[1,2,3,4,5,6,7]))
+## Solution on the GFG
+class Solution:
+	def reverseSubArray(self,arr,l,r):
+		self.helper_func(arr,l-1,r-1)
+		return arr
+		
+	def helper_func(self,arr,l,r):
+	    if l >= r:
+	        return 
+	    arr[l],arr[r] = arr[r],arr[l]
+	    self.helper_func(arr,l+1,r-1)
