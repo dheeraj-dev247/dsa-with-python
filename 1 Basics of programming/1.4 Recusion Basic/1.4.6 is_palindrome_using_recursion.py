70a7b340a06695bd96b060ca180844bfa3409bc2
dheeraj-dev247 <dheeraj.dev247@gmail.com>
2025-11-18 12:30:52 +0530

    is_palindrome using recursion is completed

--- 1 Basics of programming/1.4 Recusion Basic/1.4.6 is_palindrome_using_recursion.py
+++ 1 Basics of programming/1.4 Recusion Basic/1.4.6 is_palindrome_using_recursion.py
@@ -16,3 +16,20 @@ class Solution:
 
 # s1 = Solution()
 # print(s1.isPalindrome("wbklpwm"))
+
+
+class Solution2:
+    def is_palindrome(self, s):
+        return self.solve(s, 0, len(s) - 1)
+
+    def solve(self, s, l, r):
+        # Base condition
+        if l > r:
+            return True
+        if s[l] != s[r]:
+            return False
+        return self.solve(s, l + 1, r - 1)
+
+
+s2 = Solution2()
+print(s2.is_palindrome("wbklpwm"))
