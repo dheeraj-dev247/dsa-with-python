1a1744ffa13dbbf3b57ee8cca7a41c89e684bffd
Dheeraj Malik <160769833+dheeraj-dev247@users.noreply.github.com>
2025-11-18 12:39:31 +0000

    selection sort is done

---
+++ 2 Sorting Algorithms/2.1 selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(nums):
+    n=len(nums)
+    for i in range(0,n):
+        min_index = i
+        for j in range(i+1,n):
+            if nums[j] < nums[min_index]:
+                min_index = j
+        nums[i],nums[min_index] = nums[min_index],nums[i]
+    return nums
+
+print(selection_sort([4, 1, 3, 9, 7]))
