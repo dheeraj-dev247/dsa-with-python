a11adcbd04f4837a169a6bf53801764cdd5db063
dheeraj-dev247 <dheeraj.dev247@gmail.com>
2025-11-20 10:07:44 +0530

    merge sort is pending

---
+++ 2 Sorting Algorithms/2.4 merge_sort.py
@@ -0,0 +1,6 @@
+# Question Link:
+# https://www.geeksforgeeks.org/problems/merge-sort/1
+
+
+# Refer to the link below for detailed understanding:
+# https://codeanddebug.in/blog/merge-sort-algorithm-in-python/
